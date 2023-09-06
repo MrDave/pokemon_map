274b958725fb3f9db5894faefc90bf07467b962a
MrDave <davidschnoll@gmail.com>
2023-09-06 16:47:17 +0300

    Add verbose names in Russian to existing fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200)
-    image = models.ImageField(null=True)
+    title = models.CharField("название", max_length=200)
+    image = models.ImageField("изображение", null=True)
 
     def __str__(self):
         return f"{self.title}"
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField("Уровень")
-    health = models.IntegerField("Здоровье")
-    attack = models.IntegerField("Атака")
-    defence = models.IntegerField("Защита")
-    stamina = models.IntegerField("Выносливость")
+    pokemon = models.ForeignKey(Pokemon, verbose_name="покемон", on_delete=models.CASCADE)
+    lat = models.FloatField("широта")
+    lon = models.FloatField("долгота")
+    appeared_at = models.DateTimeField("время появления")
+    disappeared_at = models.DateTimeField("время исчезновения")
+    level = models.IntegerField("уровень")
+    health = models.IntegerField("здоровье")
+    attack = models.IntegerField("атака")
+    defence = models.IntegerField("защита")
+    stamina = models.IntegerField("выносливость")
