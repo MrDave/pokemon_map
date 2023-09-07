3933960c48c816c60a7d782841fc8bb48f6909aa
MrDave <davidschnoll@gmail.com>
2023-09-07 21:31:29 +0300

    Remove redundant defaults

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField("название (рус.)", max_length=200)
-    title_en = models.CharField("название (анг.)", max_length=200, blank=True, default="")
-    title_jp = models.CharField("название (яп.)", max_length=200, blank=True, default="")
+    title_en = models.CharField("название (анг.)", max_length=200, blank=True)
+    title_jp = models.CharField("название (яп.)", max_length=200, blank=True)
     image = models.ImageField("изображение", null=True)
-    description = models.TextField("описание", blank=True, default="")
+    description = models.TextField("описание", blank=True)
     previous_evo = models.ForeignKey("self", verbose_name="прошлая эволюция", null=True, on_delete=models.SET_NULL, related_name="next_evo")
 
     def __str__(self):
